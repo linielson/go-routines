c58868233f5c9f327065267da97371154c47cd31
Linielson <linielsonrosa@gmail.com.br>
2021-02-22 08:25:17 -0300

    Examples in Go of Wait Group and Parallelism

---
+++ concurrency/2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var waitGroup sync.WaitGroup
+
+func init() {
+	//After Go 1.5 this is not necessary
+	//For example
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	//fmt.Println(runtime.NumCPU())
+	waitGroup.Add(2)
+	go runProcess("P1", 20)
+	go runProcess("P2", 20)
+	waitGroup.Wait()
+}
+
+func runProcess(name string, total int) {
+	for i := 0; i < total; i++ {
+		fmt.Println(name, "->", i)
+		t := time.Duration(rand.Intn(255))
+		time.Sleep(time.Millisecond * t)
+	}
+	waitGroup.Done()
+}
